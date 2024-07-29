cad60ea799ee363dbe64a231811b0144d3a76e9a
cleanerzkp <0xcleaner@proton.me>
2024-07-29 23:06:37 +0200

    fix

--- app/web2/page.tsx
+++ app/web2/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import Hero from './Hero'; // Ensure this path matches where your Hero component is located
+import Hero from './hero';
 
 export default function Home() {
     const handleScroll = (id: string) => {
@@ -13,8 +13,7 @@ export default function Home() {
 
     return (
         <div className="flex flex-col min-h-[calc(100vh-4rem)]">
-            <Hero onViewProjects={() => handleScroll('features')} onContact={() => window.open('https://t.me/yourtelegram', '_blank')} /> {/* Hero Section */}
-
+            <Hero />
             <main className="flex-grow">
             </main>
         </div>
